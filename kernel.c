da4a7fa1fec4c2f2652fcbeb3a51d95ed120ea38
bigbenbigben <positive.feedback.hh@gmail.com>
2025-02-04 14:15:33 +0900

    7章のカーネルパニックを実装

--- kernel.c
+++ kernel.c
@@ -32,9 +32,12 @@ void putchar(char ch) {
 }
 
 void kernel_main(void) {
-    printf("\n\nHello %s\n", "World!");
-    printf("2 + 2 = %d, %x\n", 1 + 2, 0x1234abcd);
-    // memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
+    // printf("\n\nHello %s\n", "World!");
+    // printf("2 + 2 = %d, %x\n", 1 + 2, 0x1234abcd);
+    memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
+
+    PANIC("booted!");
+    printf("unreachable here!\n");
 
     for (;;) {
         __asm__ __volatile__("wfi");
