f131310abc325cc3a69f2fe35552a59fb1576527
bigbenbigben <positive.feedback.hh@gmail.com>
2025-02-07 17:54:18 +0900

    14章のシステムコールを実装

--- user.h
+++ user.h
@@ -1,11 +1,11 @@
 #pragma once
 #include "common.h"
 
-struct sysret {
-    int a0;
-    int a1;
-    int a2;
-};
+// struct sysret {
+//     int a0;
+//     int a1;
+//     int a2;
+// };
 
 __attribute__((noreturn)) void exit(void);
 void putchar(char ch);
